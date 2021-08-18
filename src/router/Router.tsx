797975963f275acd7e559fb9335d73b655d3e64d
sccala <sccala18@gmail.com>
2021-08-18 15:43:19 -0700

    Added routing

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -2,6 +2,7 @@ import { VFC, memo } from 'react'
 import { Route, Switch } from 'react-router-dom'
 import { Login } from '../components/pages/Login'
 import { homeRoutes } from './HomeRoutes'
+import { Page404 } from '../components/pages/Page404'
 
 export const Router: VFC = memo(() => {
   return (
@@ -14,17 +15,16 @@ export const Router: VFC = memo(() => {
         render={({ match: { url } }) => (
           <Switch>
             {homeRoutes.map((route) => (
-              <Route
-                key={route.path}
-                exact={route.exact}
-                path={`${url}${route.path}`}
-              >
+              <Route key={route.path} exact={route.exact} path={`${url}${route.path}`}>
                 {route.children}
               </Route>
             ))}
           </Switch>
         )}
       />
+      <Route path="*">
+        <Page404 />
+      </Route>
     </Switch>
   )
 })
