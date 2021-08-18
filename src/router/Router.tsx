f81c0dbcac6e0cee5453c3ac6c21da60a8fcd4d4
sccala <sccala18@gmail.com>
2021-08-18 15:35:30 -0700

    downgraded webpack to debug

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,16 +1,30 @@
 import { VFC, memo } from 'react'
 import { Route, Switch } from 'react-router-dom'
 import { Login } from '../components/pages/Login'
+import { homeRoutes } from './HomeRoutes'
 
 export const Router: VFC = memo(() => {
   return (
     <Switch>
-      <Route exact path='/'>
+      <Route exact path="/">
         <Login />
       </Route>
-      <Route path='/home' render={() => (
-        <Switch></Switch>
-      )} />
+      <Route
+        path="/home"
+        render={({ match: { url } }) => (
+          <Switch>
+            {homeRoutes.map((route) => (
+              <Route
+                key={route.path}
+                exact={route.exact}
+                path={`${url}${route.path}`}
+              >
+                {route.children}
+              </Route>
+            ))}
+          </Switch>
+        )}
+      />
     </Switch>
   )
 })
